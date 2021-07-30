b366f19c01b3da6e244f4c79f3b5dbc580add9ba
DEntisT <78434481+DEntisT-ofc@users.noreply.github.com>
2021-07-30 21:14:53 +0200

    Update first-time.py

--- first-time.py
+++ first-time.py
@@ -3,5 +3,10 @@ if name == 'Mom':
     print("I love my mom")
 elif name == 'Dad':
     print("Dad is king")
+    xd()
 else:
     print("Hi " + name)
+    xd()
+
+def xd():
+    print('Hi mommy and daddy')
